72cca7949da5b482690750e1fedd4f253ef3a148
m0ar <edvard@hubinette.me>
2024-11-20 23:15:48 +0100

    Add sourcemaps to build, disable SSR on game worker route

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-
+  build: {
+    sourcemap: true,
+    minify: false,
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
